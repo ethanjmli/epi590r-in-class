a628afc516138c31c919d85a6bc4b8ca3061c0cc
ethanjmli <ethan.jm.li@gmail.com>
2023-08-14 11:19:22 -0400

    Changed directory settings Clarified purpose of na.omit function

--- R/clean-data-bad.R
+++ R/clean-data-bad.R
@@ -4,7 +4,7 @@ nlsy_cols <- c("glasses", "eyesight", "sleep_wkdy", "sleep_wknd",
 
 #Trying commit again
 library(tidyverse)
-setwd("~/Documents/Teaching/Emory/epi590r-in-class/data/raw/")
+setwd("/Users/ethan_j_li/Library/CloudStorage/OneDrive-EmoryUniversity/Documents/R Bootcamp/epi590r-in-class/data/raw")
 nlsy <- read_csv("nlsy.csv",
 								 na = c("-1", "-2", "-3", "-4", "-5", "-998"),
 								 skip = 1, col_names = nlsy_cols)
@@ -17,7 +17,7 @@ nlsy <- nlsy |>
 				 eyesight_cat = factor(eyesight, labels = c("Excellent", "Very good", "Good", "Fair", "Poor")),
 				 glasses_cat = factor(glasses, labels = c("No", "Yes")))
 
-nlsy <- na.omit(nlsy)
+nlsy <- na.omit(nlsy) #Get rid of all rows containing at least 1 NA value
 
 setwd("../clean/")
 write_rds(nlsy, "nlsy-complete-cases.rds")
